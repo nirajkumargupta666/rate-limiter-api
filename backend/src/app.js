127ede9be8803b1463f6e9cdf29bd7ee4d67b02e
nirajkumargupta666 <nirajkumargupta666@gmail.com>
2025-06-12 17:04:25 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const rateLimiter = require('./middleware/rateLimiter');
 const routes = require('./routes');
+require('./services/mqConsumer');
 const morgan = require('morgan');
 const cors = require('cors');
 
@@ -10,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(rateLimiter);
 
 app.use('/api', routes);
 
